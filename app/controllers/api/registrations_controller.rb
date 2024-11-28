63bed94969e05e50963429db0718593ad4086181
talal-amjad0 <talal.amjad@codingcops.com>
2024-11-28 17:49:18 +0500

    Auto Signin of the prevoius user issue fixed

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -1,8 +1,13 @@
 class Api::RegistrationsController < Devise::RegistrationsController
     def create
-      super
-    rescue StandardError => e
-      render json: { error: e.message }, status: :unprocessable_entity
-    end
+        super do |resource|
+          # After the user is created, sign them in automatically
+          sign_in(resource)  # Automatically sign in after registration
+          render json: { user: resource, message: "Account created and signed in successfully" }, status: :created and return
+        end
+      rescue StandardError => e
+        # Handle any registration errors (e.g., validation errors)
+        render json: { error: e.message }, status: :unprocessable_entity
+      end
   end
   
--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -3,13 +3,17 @@ class Api::SessionsController < Devise::SessionsController
     respond_to :json
   
     def create
-      super do |resource|
-        render json: { user: resource, message: "Signed in successfully" }, status: :ok and return
+        sign_out(current_user) if current_user
+    
+        super do |resource|
+          sign_in(resource)
+          render json: { user: resource, message: "Signed in successfully" }, status: :ok and return
+        end
       end
-    end
   
     def destroy
       super do
+        sign_out(current_user) 
         render json: { message: "Signed out successfully" }, status: :ok and return
       end
     end
